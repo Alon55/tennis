33fb7155a52d87597f5b84bca76511a84793d532
alon55 <alonzager5@gmail.com>
2023-03-13 09:19:46 +0100

    blue - check 2 server scores

--- src/Tennis.test.js
+++ src/Tennis.test.js
@@ -2,30 +2,29 @@ import { fireEvent, render, screen, act, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
 import Tennis from './Tennis';
 
+const addScoresMultipleTimes = (serverTimes) => {
+    const startGameButton = screen.getByRole('button', { name: /Start Game/i });
+    const serverButton = screen.getByRole('button', { name: /Server Scores/i });
+    fireEvent.click(startGameButton)
+    serverTimes && [...Array(serverTimes)].forEach(() => fireEvent.click(serverButton))
+}
+
 describe("Tennis Scoring Tests", () => {
 
     test("Server scores", async () => {
         render(<Tennis />)
-        const startGameButton = screen.getByRole('button', { name: /Start Game/i });
-        const serverButton = screen.getByRole('button', { name: /Server Scores/i });
+        addScoresMultipleTimes(1, 0)
         const message = screen.getByTestId("message");
-        fireEvent.click(startGameButton)
-        fireEvent.click(serverButton)
 
         expect(message.innerHTML).toBe("15 - Love");
     });
 
     test("Server scores 2 times", () => {
         render(<Tennis />)
-        const startGameButton = screen.getByRole('button', { name: /Start Game/i });
-        const serverButton = screen.getByRole('button', { name: /Server Scores/i });
+        addScoresMultipleTimes(2, 0)
         const message = screen.getByTestId("message");
-        fireEvent.click(startGameButton)
-        fireEvent.click(serverButton)
-        fireEvent.click(serverButton)
 
         expect(message.innerHTML).toBe("30 - Love");
     });
-
 
 });
